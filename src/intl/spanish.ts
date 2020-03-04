3723edf8d1d4efafa58b605386abadc7acd70a13
xarnodo <58768901+xarnodo@users.noreply.github.com>
2020-03-04 23:45:27 +0100

    Create spanish.ts

---
+++ src/intl/spanish.ts
@@ -0,0 +1,526 @@
+export const englishTranslation = {
+
+
+
+
+ "createNewWallet": "Crear una nueva cartera ",
+
+
+ "generateFantomWalletText": "Genera tu cartera personal, recibe tu dirección pública, y crea las credenciales de acceso y recuperación",
+
+
+ "getStarted": "Comenzar",
+
+
+ "connectFantomNetwork": "Conéctate a la red Fantom y",
+
+
+ "sendRecieveFTM": "Envía y recibe FTM",
+
+
+ "stakeFTM": "Haz stake con tus FTM",
+
+
+ "collectRewards": "Cobra tu recompensa",
+
+
+ "accessNow": "Accede ahora",
+
+
+ "welcomeFantomWallet": "Bienvenido a FantomWallet",
+
+
+ "sendReceiveStakeFTM": "Envía, recibe y haz stake con tus FTM",
+
+
+ "address": "Dirección",
+
+
+ "balance": "Balance",
+
+
+ "createWallet": "Crea un cartera ",
+
+
+ "createKeystoreFilePassword": "Crea un archivo keystore y una contraseña",
+
+
+ "SetNewPassword": "Crea una nueva contraseña",
+
+
+ "walletPasswordValidationText": "La contraseña debe contener entre 8 y 200 caracteres, incluyendo una mayúscula, un símbolo y un número",
+
+
+ "reEnterPassword": "Introducir la contraseña de nuevo",
+
+
+ "backupKeystoreFileText": "He hecho una copia de seguridad de mi archivo keystore y he guardado la contraseña en un lugar seguro",
+
+
+ "walletCreationT&C": "Comprendo que necesitaré la contraseña y el archivo keystore para acceder a mi cartera ",
+
+
+ "passwordDoesNotMatch": "La contraseña introducida no coincide",
+
+
+ "downloadKeystoreFile": "Descargar archivo keystore",
+
+
+ "yourMnemonicPhrase": "Tu frase mnemotécnica",
+
+
+ "backupMnemonics": "Por favor, copia el texto de debajo en papel y guárdalo en un lugar secreto y seguro",
+
+
+ "viewPrivateKey": "Ver tu clave privada",
+
+
+ // "yourPrivateKey": "Tu clave privada",
+
+
+ "wroteRecoveryKey": "He escrito mi clave de recuperación",
+
+
+ "verification": "Verificación",
+
+
+ "mnemonicOrderPhrase": "Por favor selecciona las palabras en el orden correcto para verificar tu frase mnemotécnica",
+
+
+ "back": "Atrás",
+
+
+ "verify": "Verificar",
+
+
+ "youAllSet": "¡Todo listo!",
+
+
+ "successfullyCreatedWallet": "Has creado tu cartera  con éxito",
+
+
+ "accessWallet": "Accede a tu cartera ",
+
+
+ "dashboard": "Panel principal",
+
+
+ "send": "Envía",
+
+
+ "receive": "Recibe",
+
+
+ "stake": "Stake",
+
+
+ "recentActivity": "Actividad reciente",
+
+
+ "overview": "Resumen",
+
+
+ "price": "Precio",
+
+
+ "marketCap": "Cap. de mercado",
+
+
+ "deposit": "Deposita FTM",
+
+
+ "sendFTM": "Envía FTM",
+
+
+ "amount": "Cantidad",
+
+
+ "enterAmount": "Introduce la cantidad",
+
+
+ "toAddress": "A la dirección",
+
+
+ "enterAddress": "Introduce dirección",
+
+
+ "memoOptional": "Memo (opcional)",
+
+
+ "memo": "Memo",
+
+
+ "enterMemo": "Introduce memo",
+
+
+ "entireBalance": "Balance completo",
+
+
+ "copiedClipboard": "Copiado al portapapeles",
+
+
+ "clearAll": "Borrar todo",
+
+
+ "enterWalletPassword": "Por favor introduce la contraseña de tu cartera  para enviar la transacción",
+
+
+ "enterPassword": "Introduce la contraseña",
+
+
+ "invalidPassword": "Contraseña no válida",
+
+
+ "sending": "Enviando...",
+
+
+ "transactionSent!": "¡Transacción enviada!",
+
+
+ "receiveFTM": "Recibe FTM",
+
+
+ "copy": "Copia",
+
+
+ "download": "Descarga",
+
+
+ "availableStake": "Disponible para stake",
+
+
+ "currentlyStaking": "En stake actualmente",
+
+
+ "claimedRewards": "Recompensa cobrada",
+
+
+ "availableClaim": "Disponible para cobrar",
+
+
+ "whatLikeToDo?": "¿Qué te gustaría hacer?",
+
+
+ "selectValidator": "Selecciona un validador",
+
+
+ "validatorsList": "Lista de validadores",
+
+
+ "Validadores": "Validadores",
+
+
+ "clickValidator": "Haz click en un validador para más informacion",
+
+
+ "name": "Nombre",
+
+
+ "poi": "Prueba de Importancia",
+
+
+ "proofImportance": "Prueba de Importancia",
+
+
+ "validatingPower": "Poder de validación",
+
+
+ "uptime": "Tiempo activo",
+
+
+ "totalStaked": "Total en stake",
+
+
+ "spaceLeft": "Espacio disponible",
+
+
+ "commission": "Comisión",
+
+
+ "select": "Seleccionar",
+
+
+ "enterWalletPasswordStake": "Por favor, introduce la contraseña de tu cartera para hacer stake",
+
+
+ "enterWalletPasswordCommon": "Por favor, introduce tu contraseña del cartera para ",
+
+
+ "congratulations": "¡Enhorabuena!",
+
+
+ "ftmSuccessfullyStaked": "Los FTM están en stake",
+
+
+ "unstake": "Finalizar stake",
+
+
+ 'ok': 'Ok',
+
+
+ "stakeWithdrawalDays": "La retirada de fondos en stake tardará 7 días",
+
+
+ "maybeLater": "Quizás más tarde",
+
+
+ "enterWalletPasswordUnstake": "Por favor, introduce la contraseña de tu cartera para finalizar el stake",
+
+
+ "prepareWithdrawalSuccessful": "¡La preparación para retirada ha tenido éxito!",
+
+
+ "ftmAvaliableDaysMsg": "Tus 1.05 FTM estarán disponibles en 6 días y 23 horas",
+
+
+ "somethingWentWrong": "Algo ha fallado",
+
+
+ "pleaseTryAgain": "Por favor, prueba otra vez",
+
+
+ "Recipient": "Receptor",
+
+
+ "date": 'Fecha',
+
+
+ "canTransferMax":"Puedes transferir máximo",
+
+
+
+
+
+
+
+ "yourPrivateKey":"Tu clave privada",
+
+
+ "incorrectMnemonicPhrase":"Orden incorrecto de la frase mnemotécnica. Por favor prueba otra vez",
+
+
+ "transactionHash":"Hash de la transacción",
+
+
+ "invalidAmount":"Cantidad no válida",
+
+
+ "insufficentFundsTransfer":"Fondos insuficientes para la transferencia",
+
+
+ "enterValidFTMAddress":"Introduce una dirección FTM válida",
+
+
+ "notEnoughFTM":"No hay suficientes FTM. Por favor introduce una cantidad menor",
+
+
+ "invalidStakeAmount":"Cantidad para stake no válida",
+
+
+ "canStakeMax":"Cantidad máxima para stake",
+
+
+ "poiFull":"Prueba de Importancia",
+
+
+ "nodeFull":"Este nodo está lleno en este momento",
+
+
+ "diffNode":"Por favor selecciona un nodo diferente",
+
+
+ "restoreWallet":"Restaurar cartera",
+
+
+ "pleaseEnterPassword":"Por favor introduce la contraseña",
+
+
+ "pleaseUploadKeystore":"Por favor sube tu keystore",
+
+
+ "amountToStake":"Cantidad para stake",
+
+
+ "summary":"Resumen",
+
+
+ "validatorNode":"Nodo validador",
+
+
+ "withdrawalSuccessful":"Retirada con éxito",
+
+
+ "wllaet":"Los FTM están ahora en tu cartera",
+
+
+ "enterWalletPasswordModal":"Por favor, introduce la contraseña de tu cartera para",
+
+
+ "FTMAvail":"!Los FTM están disponibles para su retirada!",
+
+
+ "daysAnd":"días y",
+
+
+ "FTMAreAvailable":"FTM están disponibles",
+
+
+ "withdrawToWallet":"Retirar ahora a tu cartera",
+
+
+ "mnemonicPhrase":"Frase mnemotécnica",
+
+
+ "privateKey":"Clave privada",
+
+
+ "uploadKeystoreFile":"Sube el archivo keystore",
+
+
+ "keystoreSucessfullyLoaded":"Keystore subido con éxito",
+
+
+ "enterYourWalletPassword":"Introduce la contraseña de tu cartera",
+
+
+ "enterMnemonic":"Por favor, introduce las 12 o 24 palabras de tu frase mnemotécnica, en minúsculas, separadas por un espacio",
+
+
+ "invalidRecoveryPhrase":"Frase de recuperación incorrecta",
+
+
+ "typePrivateKey":"Por favor, teclea tu clave privada",
+
+
+ "unlockWallet":"Desbloquear cartera",
+
+
+ "enterPrivateValidation":"Por favor introduce una clave privada alfanumérica de 66 bits válida que comience con 0x",
+
+
+ "hours": 'horas',
+
+
+ 'your': 'Tu',
+
+
+ "logoutMsg":"¿Estás seguro de que quieres salir?",
+
+
+ "logoutDesc": 'Si sales se borrará la cartera del almacenamiento local. No será accesible de nuevo desde este navegador a menos que importes tu clave privada o tu frase mnemotécnica.',
+
+
+ "reverseAction": 'Esta acción es irreversible.',
+
+
+ "sent":"Enviado",
+
+
+ "fee": 'Coste',
+
+
+ "cancel": 'Cancelar',
+
+
+ "logout": 'Salir',
+
+
+ 'rewards': 'Recompensa',
+
+
+ "unstaking": 'Finalizando stake...',
+
+
+ "withdraw": 'Retirar',
+
+
+ "withdrawing": 'Retirando...',
+
+
+ "invalidKeystoreTitle": "Archivo keystore o contraseña no válidos",
+
+
+ "keystore": "Keystore",
+
+
+ "max":"Max",
+
+
+ "staking":"Stake...",
+
+
+ 'minStakeReq': 'Se requiere un stake mínimo de 1 FTM',
+
+
+ "howMuchFTM":"¿Con cuántos FTM querrías hacer stake?",
+
+
+ "accountAlreadyExist":"Ya existe una cuenta con esta dirección",
+
+
+ "minimumAmountAlert": "Necesitas mínimo",
+
+
+ "minimumAmountAlert1": "en tu balance para iniciar la transacción de retirada",
+
+
+ "minimumUnstakeAlert": "en tu balance para iniciar la transacción de retirada",
+
+
+ "browser": 'Navegador',
+
+
+ "Jan": "Ene",
+
+
+ "Feb": "Feb",
+
+
+ "Mar": "Mar",
+
+
+ "Apr": "Abr",
+
+
+ "May": "May",
+
+
+ "Jun": "Jun",
+
+
+ "Jul": "Jul",
+
+
+ "Aug": "Ago",
+
+
+ "Sept": "Sept",
+
+
+ "Sep": "Sept",
+
+
+ "Oct": "Oct",
+
+
+ "Nov": "Nov",
+
+
+ "Dec": "Dic",
+
+
+ "th":"",
+
+
+
+
+
+
+
+
+
+
+
+
+
+
